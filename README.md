a795929b7870680071ebdd3fb7bf4c401a094b0c
Ollie Killean <65198941+OllieKillean@users.noreply.github.com>
2021-12-11 01:27:06 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# 
+[![huntr](https://cdn.huntr.dev/huntr_security_badge_mono.svg)](https://huntr.dev)
+
+[![NPM](https://nodei.co/npm/@birbjs/snowflake.png)](https://nodei.co/npm/@birbjs/snowflake/)
+
+# @birbjs/snowflake
+This package allows for easy generation and interpretation of Discord snowflakes. It is mainly used for the [birb](https://npmjs.com/package/birb) package. You should already have this package installed if you are using Birb.JS.
+
+## Installation
+This package is bundled with Birb.JS. You should install Birb.JS using `npm install birb --save`.
+
+## Usage
+```js
+// Generate a snowflake from a user ID ...
+const { Snowflake } = require('@birbjs/snowflake');
+const snowflake = new Snowflake('914338137600294982');
+```
--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,8 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+let inc = 0;
+
 export class Snowflake {
 
     static DISCORD_EPOCH = 1420070400000;
@@ -17,6 +19,14 @@ export class Snowflake {
     processId: number = null!;
     incremental: number = null!;
 
+    /**
+     * @param {number} timestamp The amount of milliseconds since EPOCH
+     * @param {number} workerId The worker ID (used for generating unique snowflakes)
+     * @param {number} processId The process ID (used for generating unique snowflakes)
+     * @param {number} incremental The incremental number (used for generating unique snowflakes)
+     * @constructor
+     * @public
+     */
     constructor (timestamp: number, workerId: number, processId: number, incremental: number) {
         this.timestamp = timestamp;
         this.workerId = workerId;
@@ -24,37 +34,116 @@ export class Snowflake {
         this.incremental = incremental;
     }
 
+    /**
+     * Get the date of this snowflake.
+     * 
+     * @returns {Date} The date of this snowflake
+     * @public
+     */
     getDate (): Date {
         return new Date(this.timestamp);
     }
 
+    /**
+     * Get the worker ID of this snowflake.
+     * 
+     * @returns {number} The worker ID of this snowflake
+     * @public
+     */
     getWorkerId (): number {
         return this.workerId;
     }
 
+    /**
+     * Get the process ID of this snowflake.
+     * 
+     * @returns {number} The process ID of this snowflake
+     * @public
+     */
     getProcessId (): number {
         return this.processId;
     }
 
+    /**
+     * Get the incremental number of this snowflake.
+     * 
+     * @returns {number} The incremental number of this snowflake
+     * @public
+     */
     getIncremental (): number {
         return this.incremental;
     }
 
-    toString (): string {
-        let snowflake = `${this.timestamp}${this.workerId}${this.processId}${this.incremental}`;
-        return parseInt(snowflake).toString(2);
+    /**
+     * Do weird stuff no one understands.
+     * 
+     * @returns {BigInt} The snowflake as a big integer
+     * @public
+     */
+    toBigInt (): BigInt {
+        let snowflake = BigInt(this.timestamp - Snowflake.DISCORD_EPOCH);
+        snowflake = snowflake << 22n;
+        snowflake = snowflake | (BigInt(this.workerId) << 17n);
+        snowflake = snowflake | (BigInt(this.processId) << 12n);
+        snowflake = snowflake | BigInt(this.incremental);
+        return snowflake;
     }
 
-    static generate (worker?: number, process?: number, increment?: number): Snowflake {
+    /**
+     * Get the snowflake as a string (binary).
+     * 
+     * @returns {number} The snowflake in binary
+     * @public
+     */
+    toBinary (): string {
+        return this.toBigInt().toString(2);
+    }
+
+    /**
+     * Get the snowflake as a string.
+     * 
+     * @returns {number} The snowflake as a string
+     * @public
+     */
+    toString (): string {
+        return this.toBigInt().toString(10);
+    }
+
+    /**
+     * Get the snowflake as a compact string.
+     * 
+     * @returns {number} The snowflake as a compact string
+     * @public
+     */
+    toCompactString (): string {
+        return this.toBigInt().toString(36);
+    }
+
+    /**
+     * Generate a new snowflake.
+     * 
+     * @param {number} [worker=1] The worker ID (used for generating unique snowflakes)
+     * @param {number} [process=1] The process ID (used for generating unique snowflakes)
+     * @returns {Snowflake} The new snowflake
+     * @static
+     */
+    static generate (worker?: number, process?: number): Snowflake {
         let timestamp = Date.now();
         let workerId = worker !== undefined ? worker : 1;
         let processId = process !== undefined ? process : 1;
-        let incremental = process !== undefined ? process : Math.floor(Math.random() * 10);
+        let incremental = inc++;
         return new Snowflake(timestamp, workerId, processId, incremental);
     }
 
+    /**
+     * Convert a string or number to a snowflake.
+     * 
+     * @param {string | number} string The string or number to convert
+     * @returns {Snowflake} The new snowflake
+     * @static
+     */
     static fromString (string: string | number): Snowflake {
-        let snowflake = BigInt(string);
+        let snowflake = BigInt(string.toString());
         let timestamp = Number(snowflake >> 22n) + Snowflake.DISCORD_EPOCH;
         let workerId = Number((snowflake >> 17n) & 0b11111n);
         let processId = Number((snowflake >> 12n) & 0b11111n);
